5301a2dacc8ec8bb81c1116befc7df9c32527e48
pccgaucho <pccgaucho@gmail.com>
2023-07-18 13:37:35 -0300

    Configuração do ambiente para projeto final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='p-6 bg-gray-400 border-b-orange-500'>
+        <h1 className='text-orange-500 font-bold'>Ola</h1>
+      </div>
+    </>
+   
+  )
+}
+
+export default App
